d06f99599a9b44284f6a0f3be704159280bb233a
Agrippine DALMEIDA <ddalmeida@et.esiea.fr>
2020-05-06 23:04:50 +0200

    API Rest

--- app/src/main/java/com/example/tdcodelab/MainActivity.java
+++ app/src/main/java/com/example/tdcodelab/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.tdcodelab;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co/";
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -23,13 +31,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mAdapter = new Adapter(input);
-//        recyclerView.setAdapter(mAdapter);
-
-    showList()
-
-
-}
+        showList();
+        makeApiCall();
+    }
 
     private void showList() {
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
@@ -39,10 +43,12 @@ public class MainActivity extends AppCompatActivity {
         List<String> input = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             input.add("Wave" + i);
+        }
+        mAdapter = new Adapter(input);
+        recyclerView.setAdapter(mAdapter);
+
     }
 
-
-    private static final String BASE_URL = "https://pokeapi.co/";
     private void makeApiCall() {
         Gson gson = new GsonBuilder()
                 .setLenient()
@@ -56,6 +62,18 @@ public class MainActivity extends AppCompatActivity {
         PokeApi pokeApi = retrofit.create(PokeApi.class);
 
         Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
-//        call.enqueue(this);
+        call.enqueue(new Callback<RestPokemonResponse>() {
+            @Override
+            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                if(response.isSuccessful()){
+                    List<Pokemon> pokemonList =
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
